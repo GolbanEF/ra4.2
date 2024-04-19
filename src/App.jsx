22730f0bfe32b0c3b33726f9ea0fbae22166fbdc
GolbanEF <Golban.ef@yandex.ru>
2024-04-19 23:45:42 +0300

    go

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import StepCounter from './components/StepCounter';
+
+function App() {
+  return (
+    <div className="App">
+      <StepCounter />
+    </div>
+  );
+}
+
+export default App;
